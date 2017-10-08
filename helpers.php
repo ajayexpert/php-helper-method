c110bebddf256cceef321aea0c51cbace3446778
Ajay Kumar <ajay.expert@live.com>
2017-10-08 16:25:47 +0530

    updated

--- helpers.php
+++ helpers.php
@@ -339,7 +339,7 @@ function ifsetEqual(&$var, $value) {
 |
 */
 
-function isset_multi($search,$array){
+function isset_multi($search, $array){
 	$array = is_object($array) ? json_decode(json_encode($array), true) : $array;
 
 	$return = (
@@ -367,12 +367,19 @@ function is_url_exist($url){
 }
 
 
-function implode_kv($array){
-	return implode(', ', array_map(
-		function ($v, $k) { return sprintf("%s='%s'", $k, $v); },
-		$array,
-		array_keys($array)
-	));
+function parameterize_array($array, $glue = '=', bool $isReverse = false) {
+    $out = [];
+
+    foreach($array as $key => $value){
+      $out[] = $isReverse ? $value.$glue.$key : $key.$glue.$value;
+    }
+
+    return $out;
+}
+
+
+function implode_kv($glue, $keyGlue, Array $array){
+	return implode($glue, parameterize_array($array, $keyGlue));
 }
 
 
@@ -447,12 +454,11 @@ function httpPost($url, $data = null,  $header = [])
 
 function currencyExchange($From = '', $To = ''){
 
-
 	$url = ($From == '' || $To == '')
 
-			 ? 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22'.$From.$To.'%22)&env=store://datatables.org/alltableswithkeys'
+			 ? 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDEUR%22,%20%22USDJPY%22,%20%22USDBGN%22,%20%22USDCZK%22,%20%22USDDKK%22,%20%22USDGBP%22,%20%22USDHUF%22,%20%22USDLTL%22,%20%22USDLVL%22,%20%22USDPLN%22,%20%22USDRON%22,%20%22USDSEK%22,%20%22USDCHF%22,%20%22USDNOK%22,%20%22USDHRK%22,%20%22USDRUB%22,%20%22USDTRY%22,%20%22USDAUD%22,%20%22USDBRL%22,%20%22USDCAD%22,%20%22USDCNY%22,%20%22USDHKD%22,%20%22USDIDR%22,%20%22USDILS%22,%20%22USDINR%22,%20%22USDKRW%22,%20%22USDMXN%22,%20%22USDMYR%22,%20%22USDNZD%22,%20%22USDPHP%22,%20%22USDSGD%22,%20%22USDTHB%22,%20%22USDZAR%22,%20%22USDISK%22)&env=store://datatables.org/alltableswithkeys'
 
-			 : 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDEUR%22,%20%22USDJPY%22,%20%22USDBGN%22,%20%22USDCZK%22,%20%22USDDKK%22,%20%22USDGBP%22,%20%22USDHUF%22,%20%22USDLTL%22,%20%22USDLVL%22,%20%22USDPLN%22,%20%22USDRON%22,%20%22USDSEK%22,%20%22USDCHF%22,%20%22USDNOK%22,%20%22USDHRK%22,%20%22USDRUB%22,%20%22USDTRY%22,%20%22USDAUD%22,%20%22USDBRL%22,%20%22USDCAD%22,%20%22USDCNY%22,%20%22USDHKD%22,%20%22USDIDR%22,%20%22USDILS%22,%20%22USDINR%22,%20%22USDKRW%22,%20%22USDMXN%22,%20%22USDMYR%22,%20%22USDNZD%22,%20%22USDPHP%22,%20%22USDSGD%22,%20%22USDTHB%22,%20%22USDZAR%22,%20%22USDISK%22)&env=store://datatables.org/alltableswithkeys';
+			 : 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22'.$From.$To.'%22)&env=store://datatables.org/alltableswithkeys';
 
 	// fatching response here
 	$response = httpGet($url);
@@ -484,6 +490,12 @@ function removeLeadingZero($value=0)
 // 
 function sub_string($string, $length = 50, $start = 0){
 	return strlen($string) >= $length ? substr($string,$start,$length).'...' : $string; 
+}
+
+
+function array_intval(Array $array)
+{
+	return array_map('intval', $array);
 }
 
 
