9a16842f4235313baf1cc2e19b7cc442ffa67bb4
Ajay Kumar <ajay.expert@live.com>
2017-10-26 15:27:29 +0530

    added remote file checking method

--- helpers.php
+++ helpers.php
@@ -230,6 +230,24 @@ function list_folder_files($dir){
 }
 
 
+function checkRemoteFile($url)
+{
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,$url);
+	// don't download content
+	curl_setopt($ch, CURLOPT_NOBODY, 1);
+	curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	if(curl_exec($ch)!==FALSE)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
 /*
  * Convert an integer to a string of uppercase letters (A-Z, AA-ZZ, AAA-ZZZ, etc.)
  */
